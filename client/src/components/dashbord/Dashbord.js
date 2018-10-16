8da40f8562b89bc6873cac6b5a51f631f9871f1e
Hassanbhb <bussinesbhb@gmail.com>
2018-10-16 15:16:59 +0100

    fixed NewPoll and Dashbord components

--- client/src/components/dashbord/Dashbord.js
+++ client/src/components/dashbord/Dashbord.js
@@ -7,7 +7,7 @@ class Dashbord extends Component {
         <div className="container">
           <div className="collection with-header">
             <h1 className="collection-header">My Polls</h1>
-            <a href="#!" className="collection-item"><span className="badge">1 votes</span><i class="fas fa-chart-pie"></i> My polls title</a>
+            <a href="#!" className="collection-item"><span className="badge">1 votes</span><i className="fas fa-chart-pie"></i> My polls title</a>
           </div>
         </div>
       </div>
--- client/src/components/newPoll/NewPoll.js
+++ client/src/components/newPoll/NewPoll.js
@@ -1,34 +1,61 @@
 import React, { Component } from 'react'
 
 class NewPoll extends Component {
+
+  createOption = (e) => {
+    const options = document.getElementById('options')
+    if(options.children.length === 10){
+      alert('you are only allowed to 10 options per chart')
+    }else{
+      //create div element
+      const div = document.createElement('div')
+      div.setAttribute('class', 'input-field col s6 m6')
+      //create input
+      const input = document.createElement('input')
+      input.setAttribute('id', 'option')
+      input.setAttribute('type', 'text')
+      //create label
+      const label = document.createElement('label')
+      const textNode = document.createTextNode('option')
+      label.setAttribute('for', 'option')
+      label.appendChild(textNode)
+      //append input and label to div
+      div.appendChild(input)
+      div.appendChild(label)
+      //append div to options div
+      options.appendChild(div);
+    }
+  }
+
   render() {
     return (
       <div>
-      <div onSubmit={this.submit} className="row">
-            <form className="col s12 offset-m3">
+        <div onSubmit={this.submit} className="row">
+            <form className="col s12 center">
               <h1>Create new Chart</h1>
               <div className="row">
-                <div className="input-field col s12 m6">
+                <div className="input-field offset-m3 col s12 m6">
                   <input id="chart_title" type="text" required/>
                   <label htmlFor="chart_title">Title</label>
                 </div>
               </div>
-              <div className="row">
-                <div className="input-field col s12 m2">
-                  <input id="chart_option" type="text" required/>
-                  <label htmlFor="chart_option">Option</label>
+                
+              <div id="options" className="row">
+                <div className="input-field col s6 m6">
+                  <input id="option" type="text"/>
+                  <label htmlFor="option">option</label>
                 </div>
-                <div className="input-field col s12 m2">
-                  <input id="chart_option" type="text" required/>
-                  <label htmlFor="chart_option">Option</label>
+                <div className="input-field col s6 m6">
+                  <input id="option" type="text"/>
+                  <label htmlFor="option">option</label>
                 </div>
-                <div className="input-field col s12 m2">
-                  <input id="chart_option" type="text" required/>
-                  <label htmlFor="chart_option">Option</label>
-                </div>
-              </div> 
+              </div>
               
-              <button className="btn waves-effect waves-light" type="submit" name="action">Create Chart</button>
+              <div className="col s12 m12">
+                <p onClick={this.createOption} className="btn-floating btn-large waves-effect waves-light red"><i className="fas fa-plus"></i></p><br/>
+                <button className="btn waves-effect waves-light" type="submit" name="action">Create Chart</button>
+              </div>
+              
             </form>
           </div>
       </div>
