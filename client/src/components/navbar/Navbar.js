6596d57d3f93d953e817a607a22f0810efb22bb1
Hassanbhb <bussinesbhb@gmail.com>
2018-10-16 15:12:52 +0100

    fixed navigation on mobile

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Navbar() {
-  return (
-    <div>
-      <nav>
-        <div className="nav-wrapper blue lighten-3">
-          <Link to="/" className="brand-logo">YouVote</Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><Link to="/Dashbord">Dashbord</Link></li>
-            <li><Link to="/newpoll">New Poll</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-    
-  )
+function Navbar (){
+
+    return (
+      <div>
+        <nav>
+          {/* the navs materialize initialization is in public/index.html */}
+          <div className="nav-wrapper blue lighten-3">
+            <Link to="/" className="brand-logo">YouVote</Link>
+            <a data-target="mobile-demo" className="sidenav-trigger right"><i className="fas fa-bars"></i></a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><Link to="/Dashbord">Dashbord</Link></li>
+              <li><Link to="/newpoll">New Poll</Link></li>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/signup">Signup</Link></li>
+            </ul>
+          </div>
+        </nav>
+  
+        <ul className="sidenav" id="mobile-demo">
+          <li><Link to="/Dashbord">Dashbord</Link></li>
+          <li><Link to="/newpoll">New Poll</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Signup</Link></li>
+        </ul>
+      </div>
+      
+    ) 
 }
 
 export default Navbar;
