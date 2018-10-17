387bbe2d7e12a81c72ba168b014e6bdb4b0ae7ba
Hassanbhb <bussinesbhb@gmail.com>
2018-10-17 19:00:37 +0100

    added login route and linked it to client

---
+++ server/auth/index.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const Joi = require('joi');
+const Users = require('../models/user');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const router = express.Router();
+
+const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email({ minDomainAtoms: 2 }),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).required()
+    // - at least 12 characters
+    // - can contain letters or numbers
+})
+
+router.get('/', (req, res)=>{
+
+    res.send({
+        message: 'hello router'
+    })
+})
+
+router.post('/signup', (req, res, next)=>{
+    
+    const result = Joi.validate(req.body, schema)
+    if(result.error === null){
+        //make sure username is unique
+        Users.findOne({username: req.body.username})
+            .then( user => {
+                //if user exists then username is a duplicate
+                if(user){
+                    //respond with error
+                    const err = new Error('username already taken, try another one')
+                    next(err);
+                }else{ // else it's unique
+                    //hash the password
+                    bcrypt.genSalt(12, (err, salt) => {
+                        bcrypt.hash(req.body.password, salt, (err, hash) => {
+                            //create a new user with the hashed password
+                            const newUser = new Users({
+                                username: req.body.username,
+                                email: req.body.email,
+                                password: hash
+                            })
+                            // save to db
+                            newUser.save().then(user => {
+                                res.json({
+                                    message: 'successful sign up'
+                                })
+                            }).catch(err => {
+                                next(new Error('something went wrong'))
+                            })
+                        })
+                    })
+                }
+                
+            })
+    }else{
+        //send error
+        next(result.error)
+    }
+    
+})
+
+router.post('/login', (req, res, next) => {
+
+    const result = Joi.validate({ username: req.body.username, password: req.body.password}, schema)
+	if(result.error === null){
+		//search this username
+        Users.findOne({username: req.body.username})
+            .then(user => {
+				//if found
+                if(user){
+					// compare the passwords
+                    if(bcrypt.compare(req.body.password, user.password)){
+                        //if they match then create a payload and use to generate the token
+                        const payload = {
+							_id: user._id,
+							username: user.username
+                        }
+                        //generate token
+                        jwt.sign(payload, process.env.TOKEN_SECRET, {
+                            expiresIn: '1d'
+                        }, (err, token) => {
+                            if(err){
+                                respondError422(res, next)
+                            }else{
+                                res.json({
+                                    token
+                                })
+                            }
+                        })
+                    }else{
+						res.json({
+							'error': 'wrong user name and password'
+						})
+					}
+                }else{
+					//if not found send error
+                    res.json({
+                        'error': 'wrong user name and password'
+                    })
+                }
+            })
+    }else{
+		next(result.error);
+	}
+    
+})
+
+module.exports = router;
