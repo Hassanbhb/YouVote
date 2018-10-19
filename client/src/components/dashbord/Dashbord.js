e94a6749a66cb539586a81ef506b8ef98d5d07ac
Hassanbhb <bussinesbhb@gmail.com>
2018-10-19 20:22:28 +0100

    get users data, rediredct if not authorized

--- client/src/components/dashbord/Dashbord.js
+++ client/src/components/dashbord/Dashbord.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 
 class Dashbord extends Component {
+  state = {
+    user: {}
+  }
+  
+  componentDidMount(){
+    fetch('http://localhost:8080/', {
+     headers: {
+       authorization: "Bearer "+ localStorage.token
+     }
+    })
+    .then(res => res.json())
+    .then(result => {
+      if(result.user){
+        this.setState({
+          user: {
+            ...result.user
+          }
+        })
+      }else{
+        localStorage.removeItem('token');
+        this.props.history.push('/login')
+      }
+      
+    })
+    .catch(err => console.log(err));
+    // if(localStorage.token){
+    //   this.props.history.push('/dashbord')
+    // }else{
+    //   this.props.history.push('/login')
+    // }
+  }
   render() {
     return (
       <div>
         <div className="container">
+          <h4>Username: {this.state.user.username}</h4>
           <div className="collection with-header">
             <h1 className="collection-header">My Polls</h1>
             <a href="#!" className="collection-item"><span className="badge">1 votes</span><i className="fas fa-chart-pie"></i> My polls title</a>
@@ -15,4 +48,4 @@ class Dashbord extends Component {
   }
 }
 
-export default Dashbord;
+export default withRouter(Dashbord);
