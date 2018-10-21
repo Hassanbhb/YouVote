0ba3eaae425fda3379585cceefb3a024cbbfb582
Hassanbhb <bussinesbhb@gmail.com>
2018-10-21 14:52:51 +0100

    added signed in and out links

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SignedInLinks from './SignedInLinks'
+import SignedOutLinks from './SignedOutLinks'
 
 function Navbar (){
 
@@ -11,19 +13,15 @@ function Navbar (){
             <Link to="/" className="brand-logo">YouVote</Link>
             <a href="#!" data-target="mobile-demo" className="sidenav-trigger right"><i className="fas fa-bars"></i></a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><Link to="/dashbord">Dashbord</Link></li>
-              <li><Link to="/newpoll">New Poll</Link></li>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/signup">Signup</Link></li>
+              <SignedInLinks />
+              <SignedOutLinks />
             </ul>
           </div>
         </nav>
   
         <ul className="sidenav" id="mobile-demo">
-          <li><Link to="/Dashbord">Dashbord</Link></li>
-          <li><Link to="/newpoll">New Poll</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/signup">Signup</Link></li>
+          <SignedInLinks />
+          <SignedOutLinks />
         </ul>
       </div>
       
