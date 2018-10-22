47b33432ed39e1527a9e65bc5651bd90cdf88fd3
Hassanbhb <bussinesbhb@gmail.com>
2018-10-22 16:19:50 +0100

    added the auth service to talk to server

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,129 +1,119 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
-import Joi from 'joi'
-import { toast } from 'react-toastify'
-import Loading from '../Loading'
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Joi from "joi";
+import { toast } from "react-toastify";
+import Loading from "../Loading";
+import loginUser from "../../services/authService";
 
 class Login extends Component {
-
   state = {
     logginIn: true,
-    user:{
-      username: '',
-      password: ''
+    user: {
+      username: "",
+      password: ""
     }
-  }
+  };
 
-  componentDidMount(){
-    if(localStorage.token){
-      this.props.history.push('/dashbord')
-    }
-  }
-
-  getInput = (e) => {
+  getInput = e => {
     this.setState({
       user: {
         ...this.state.user,
         [e.target.name]: e.target.value
       }
-    })
-  }
+    });
+  };
 
-  submit = (e) => {
+  submit = e => {
     e.preventDefault();
 
-    if(this.validate()){
-      fetch('http://localhost:8080/auth/login', {
-      method: 'POST',
-      body: JSON.stringify(this.state.user),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => {
-      if(res.ok){
-        return res.json()
-      }
-      //if server sends error throw it to the catch
-      return res.json().then(err => {
-        throw new Error(err.message)
-      })
-    })
-    .then(response => {
-      //login success
-      this.setState({
-        logginIn: false
-      })
-      localStorage.token = response.token
-      toast.success('Welcome Back!', {
-        position: toast.POSITION.BOTTOM_RIGHT
+    if (this.validate()) {
+      loginUser("POST", "auth/login", this.state.user).then(response => {
+        console.log(response);
       });
-      setTimeout(() => {
-        this.props.history.push('/dashbord')
-      }, 1000);
-    })
-    .catch(err => {
-      //login failed
-      toast.error(`${err.message}`, {
-        position: toast.POSITION.BOTTOM_RIGHT
-      });
-    });
-    } 
-  }
+    }
+  };
 
   validate = () => {
     const schema = Joi.object().keys({
-      username: Joi.string().alphanum().min(3).max(30).required(),
-      password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).required()
-
-    })
+      username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+      password: Joi.string()
+        .regex(/^[a-zA-Z0-9]{8,30}$/)
+        .required()
+    });
     //validate data
     const result = Joi.validate(this.state.user, schema);
-    if(result.error === null){
-      return true
+    if (result.error === null) {
+      return true;
     }
 
-    if(result.error.message.includes('username')){
-      toast.error('Error: invalid username !', {
+    if (result.error.message.includes("username")) {
+      toast.error("Error: invalid username !", {
         position: toast.POSITION.BOTTOM_RIGHT
       });
-    }else{
-      toast.error('Error: invalid password !', {
+    } else {
+      toast.error("Error: invalid password !", {
         position: toast.POSITION.BOTTOM_RIGHT
       });
     }
-    
-  }
-
+  };
 
   render() {
     return (
-        <React.Fragment>
-          <div className="center">
-          { this.state.logginIn ? 
+      <React.Fragment>
+        <div className="center">
+          {this.state.logginIn ? (
             <div className="row">
               <form onSubmit={this.submit} className="col s12 m6 offset-m3">
                 <h1>Login</h1>
                 <div className="row">
                   <div className="input-field col s12">
-                    <input id="user_name" type="text" name="username" onChange={this.getInput} required/>
+                    <input
+                      id="user_name"
+                      type="text"
+                      name="username"
+                      onChange={this.getInput}
+                      required
+                    />
                     <label htmlFor="user_name">Username</label>
-                    <span className="helper-text">Only numbers and letters</span>
+                    <span className="helper-text">
+                      Only numbers and letters
+                    </span>
                   </div>
                 </div>
                 <div className="row">
                   <div className="input-field col s12">
-                    <input id="user_password" type="password" name="password" onChange={this.getInput} required/>
+                    <input
+                      id="user_password"
+                      type="password"
+                      name="password"
+                      onChange={this.getInput}
+                      required
+                    />
                     <label htmlFor="user_password">Password</label>
-                    <span className="helper-text">Must be 12 characters, letters and numbers</span>
+                    <span className="helper-text">
+                      Must be 12 characters, letters and numbers
+                    </span>
                   </div>
-                </div>  
-                <button className="btn waves-effect waves-light" type="submit" name="action">Login</button>
+                </div>
+                <button
+                  className="btn waves-effect waves-light"
+                  type="submit"
+                  name="action"
+                >
+                  Login
+                </button>
               </form>
-            </div> : <Loading /> }
-          </div>
-        </React.Fragment>
-    )
+            </div>
+          ) : (
+            <Loading />
+          )}
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
--- client/src/components/signup/SignUp.js
+++ client/src/components/signup/SignUp.js
@@ -1,151 +1,160 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
-import Joi from 'joi'
-import { toast } from 'react-toastify'//alerts
-import Loading from '../Loading'
-
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Joi from "joi";
+import { toast } from "react-toastify"; //alerts
+import Loading from "../Loading";
+import { connect } from "react-redux";
+import { createUser } from "../../store/actions/userActions";
+import saveUser from "../../services/authService";
 
 class SignUp extends Component {
-
   state = {
     signingUp: true,
-    user:{
-      username: '',
-      email: '',
-      password: ''
+    user: {
+      username: "",
+      email: "",
+      password: ""
     }
-  }
+  };
 
-  //if client is logged in send him to dashbord
-  componentDidMount(){
-    if(localStorage.token){
-      this.props.history.push('/dashbord')
-    }
-  }
-
-  getInput = (e) =>{
+  getInput = e => {
     this.setState({
-      user:{
+      user: {
         ...this.state.user,
         [e.target.name]: e.target.value
       }
-    })
-  }
+    });
+  };
 
-  submit = (e) => {
-    e.preventDefault()
+  submit = e => {
+    e.preventDefault();
 
     //if input validated
     if (this.validate()) {
-      //send to server
-      fetch("http://localhost:8080/auth/signup", {
-        method: "POST",
-        body: JSON.stringify(this.state.user),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-        .then(res => {
-          if(res.ok){
-            return res.json();
-          }
-          //if res is not 200
-          return res.json().then(err => {
-            throw new Error(err.message)
-          })
-        }).then((response) => {
-          //change the state to display the loader  
-          this.setState({
-            signingUp: false
-          })
-          localStorage.token = response.token;
-          toast.success('success: Welcome !', {
-            position: toast.POSITION.BOTTOM_RIGHT,
-            autoClose: 1500
-          });
-          //wait 800 milliseconds then redirect
-          setTimeout(() => {
-            this.props.history.push('/dashbord')
-          }, 1000);
-        })
-        .catch(err => {
-          //if err (username is a duplicate)
-          toast.error(`${err}`, {
-            position: toast.POSITION.BOTTOM_RIGHT
-          });
-        });
+      saveUser("POST", "auth/signup", this.state.user).then(result => {
+        console.log(result);
+      });
     }
-    
-  }
+  };
 
   //validate data with joi
   validate = () => {
     //set schema
     const schema = Joi.object().keys({
-      username: Joi.string().alphanum().min(3).max(30).required(),
-      email: Joi.string().email({ minDomainAtoms: 2 }).required(),
-      password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).required()
-    })
+      username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+      email: Joi.string()
+        .email({ minDomainAtoms: 2 })
+        .required(),
+      password: Joi.string()
+        .regex(/^[a-zA-Z0-9]{8,30}$/)
+        .required()
+    });
     //check if the state is valid
     const result = Joi.validate(this.state.user, schema);
-    if(result.error === null){
+    if (result.error === null) {
       //if yes then retur true
-      return true
+      return true;
     }
 
     //if error has a keyword then sed error for that keyword
-    if(result.error.message.includes('username')){
-      toast.error('Error: invalid username !', {
+    if (result.error.message.includes("username")) {
+      toast.error("Error: invalid username !", {
         position: toast.POSITION.BOTTOM_RIGHT
       });
-
-    }else if(result.error.message.includes('password')){
-      toast.error('Error: invalid password !', {
+    } else if (result.error.message.includes("password")) {
+      toast.error("Error: invalid password !", {
         position: toast.POSITION.BOTTOM_RIGHT
       });
-    }else{
-      toast.error('Error: invalid email !', {
+    } else {
+      toast.error("Error: invalid email !", {
         position: toast.POSITION.BOTTOM_RIGHT
       });
     }
-  }
+  };
 
   render() {
     return (
       <React.Fragment>
-          <div className="container center-align active row">
-            
-            { this.state.signingUp ? 
-            
-              <form onSubmit={this.submit} className="col s12 m6 offset-m3">
-                <h1>Sign Up</h1>
-                <div className="row">
-                  <div className="input-field col s12">
-                    <input id="user_name" type="text" name='username' onChange={this.getInput} required/>
-                    <label htmlFor="user_name">Username</label>
-                    <span className="helper-text">Only numbers and letters</span>
-                  </div>
+        <div className="container center-align active row">
+          {this.state.signingUp ? (
+            <form onSubmit={this.submit} className="col s12 m6 offset-m3">
+              <h1>Sign Up</h1>
+              <div className="row">
+                <div className="input-field col s12">
+                  <input
+                    id="user_name"
+                    type="text"
+                    name="username"
+                    onChange={this.getInput}
+                    required
+                  />
+                  <label htmlFor="user_name">Username</label>
+                  <span className="helper-text">Only numbers and letters</span>
                 </div>
-                <div className="row">
-                  <div className="input-field col s12">
-                    <input id="user_email" type="email" name="email" onChange={this.getInput} required/>
-                    <label htmlFor="user_email">Email</label>
-                  </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s12">
+                  <input
+                    id="user_email"
+                    type="email"
+                    name="email"
+                    onChange={this.getInput}
+                    required
+                  />
+                  <label htmlFor="user_email">Email</label>
                 </div>
-                <div className="row">
-                  <div className="input-field col s12">
-                    <input id="user_password" type="password" name="password" onChange={this.getInput} required/>
-                    <label htmlFor="user_password">Password</label>
-                    <span className="helper-text">Must be 12 characters, letters and numbers</span>
-                  </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s12">
+                  <input
+                    id="user_password"
+                    type="password"
+                    name="password"
+                    onChange={this.getInput}
+                    required
+                  />
+                  <label htmlFor="user_password">Password</label>
+                  <span className="helper-text">
+                    Must be 12 characters, letters and numbers
+                  </span>
                 </div>
-                <button className="btn waves-effect waves-light" type="submit" name="action">Submit</button>
-              </form> : <Loading />
-              }
-          </div>  
+              </div>
+              <button
+                className="btn waves-effect waves-light"
+                type="submit"
+                name="action"
+              >
+                Submit
+              </button>
+            </form>
+          ) : (
+            <Loading />
+          )}
+        </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default withRouter(SignUp);
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createUser: user => {
+      dispatch(createUser(user));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(SignUp));
