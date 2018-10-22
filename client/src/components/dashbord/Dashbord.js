2b329750c7cf632d33c18787bcd8e9c46f8810d5
Hassanbhb <bussinesbhb@gmail.com>
2018-10-22 16:23:42 +0100

    check if token exists if not redirect

--- client/src/components/dashbord/Dashbord.js
+++ client/src/components/dashbord/Dashbord.js
@@ -1,51 +1,64 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
 class Dashbord extends Component {
-  state = {
-    user: {}
-  }
-  
-  componentDidMount(){
-    fetch('http://localhost:8080/', {
-     headers: {
-       authorization: "Bearer "+ localStorage.token
-     }
-    })
-    .then(res => res.json())
-    .then(result => {
-      if(result.user){
-        this.setState({
-          user: {
-            ...result.user
-          }
-        })
-      }else{
-        localStorage.removeItem('token');
-        this.props.history.push('/login')
-      }
-      
-    })
-    .catch(err => console.log(err));
-    // if(localStorage.token){
-    //   this.props.history.push('/dashbord')
-    // }else{
-    //   this.props.history.push('/login')
-    // }
+  componentDidMount() {
+    const { user } = this.props;
+    if (Object.keys(user).length === 0) {
+      this.props.history.push("/login");
+    } else {
+      console.log(user);
+    }
+    //   fetch("http://localhost:8080/", {
+    //     headers: {
+    //       authorization: "Bearer " + localStorage.token
+    //     }
+    //   })
+    //     .then(res => res.json())
+    //     .then(result => {
+    //       console.log(result);
+    //       if (result.user) {
+    //         this.setState({
+    //           user: {
+    //             ...result.user
+    //           }
+    //         });
+    //       } else {
+    //         localStorage.removeItem("token");
+    //         this.props.history.push("/login");
+    //       }
+    //     })
+    //     .catch(err => console.log(err));
+    //   // if(localStorage.token){
+    //   //   this.props.history.push('/dashbord')
+    //   // }else{
+    //   //   this.props.history.push('/login')
+    //   // }
   }
   render() {
+    const { user } = this.props;
     return (
       <div>
         <div className="container">
-          <h4>Username: {this.state.user.username}</h4>
+          <h4>Username: {user.username}</h4>
           <div className="collection with-header">
             <h1 className="collection-header">My Polls</h1>
-            <a href="#!" className="collection-item"><span className="badge">1 votes</span><i className="fas fa-chart-pie"></i> My polls title</a>
+            <a href="#!" className="collection-item">
+              <span className="badge">1 votes</span>
+              <i className="fas fa-chart-pie" /> My polls title
+            </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default withRouter(Dashbord);
+const mapStateToProps = state => {
+  return {
+    user: state.auth.user
+  };
+};
+
+export default connect(mapStateToProps)(withRouter(Dashbord));
